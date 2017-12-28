3b878daeaae86228f3649e4d11528c97dd1de22f
Nidish Ramakrishnan <nidishkrishnan45@gmail.com>
2017-12-28 15:33:02 +0530

    Refactored crowdsale contracts

--- contracts/crowdsale/multistage/Crowdsale.sol
+++ contracts/crowdsale/multistage/Crowdsale.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.11;
 
-import '../../token/Token.sol';
-import '../../SafeMath.sol';
+import '../CrowdsaleBase.sol';
 
 /**
  * @title Crowdsale
@@ -11,46 +10,22 @@ import '../../SafeMath.sol';
  * on a token per ETH rate. Funds collected are forwarded to a wallet
  * as they arrive.
  */
-contract Crowdsale {
-  using SafeMath for uint256;
+contract Crowdsale is CrowdsaleBase {
 
   struct Rate {
     uint256 end;
     uint256 swapRate;
   }
 
-  // The token being sold
-  address public tokenAddr;
-
-  // start and end timestamps where investments are allowed (both inclusive)
-  uint256 public startTime;
-  uint256 public endTime;
-
-  // address where funds are collected
-  address public wallet;
-
   // how many token units a buyer gets per wei
   Rate[15] internal rate;
 
-  uint256 public weiRaised;
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-
-  function Crowdsale(uint256 _startTime, uint256[] _ends, uint256[] _swapRate, address _tokenAddr, address _wallet) public {
-    require(_wallet != address(0));
+  function Crowdsale(uint256 _startTime, uint256[] _ends, uint256[] _swapRate, address _tokenAddr, address _wallet) public
+    CrowdsaleBase(_startTime, _tokenAddr, _wallet)
+  {
     require(_ends.length == _swapRate.length);
     require(_ends[0] > _startTime);
 
-    tokenAddr = _tokenAddr;
-    wallet = _wallet;
-    startTime = _startTime;
     endTime = _ends[_ends.length - 1];
 
     for(uint8 i = 0; i < _ends.length; i++) {
@@ -71,46 +46,8 @@ contract Crowdsale {
     }
   }
 
-  // fallback function can be used to buy tokens
-  function () external payable {
-    buyTokens(msg.sender);
-  }
-
-  // low level token purchase function
+  // low level tokenAddr purchase function
   function buyTokens(address beneficiary) public payable {
-    require(beneficiary != address(0));
-    require(validPurchase());
-
-    uint256 weiAmount = msg.value;
-
-    // calculate token amount to be created
-    uint256 tokens = weiAmount.mul(currentRate());
-
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
-
-    Token(tokenAddr).mint(beneficiary, tokens);
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
-
-    forwardFunds();
+    _buyTokens(beneficiary, currentRate());
   }
-
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
-  function forwardFunds() internal {
-    require(wallet.call.gas(2000).value(msg.value)());
-  }
-
-  // @return true if the transaction can buy tokens
-  function validPurchase() internal constant returns (bool) {
-    bool withinPeriod = now >= startTime && now <= endTime;
-    bool nonZeroPurchase = msg.value != 0;
-    return withinPeriod && nonZeroPurchase;
-  }
-
-  // @return true if crowdsale event has ended
-  function hasEnded() public constant returns (bool) {
-    return now > endTime;
-  }
-
 }
