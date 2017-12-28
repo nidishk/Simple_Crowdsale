6ca32d07724dab1c831c710d6dd9dd9d45db5ebc
Nidish Ramakrishnan <nidishkrishnan45@gmail.com>
2017-12-28 15:33:44 +0530

    Added test case for whitelist

---
+++ test/whiteList.js
@@ -0,0 +1,28 @@
+const BigNumber = web3.BigNumber
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+import EVMThrow from './helpers/EVMThrow'
+
+const WhiteList = artifacts.require('WhiteList')
+
+contract('WhiteList', function ([_, owner, wallet, investor]) {
+
+  beforeEach(async function () {
+    this.list = await WhiteList.new({from: owner})
+  })
+
+
+  it('should accept whiteListing by owner', async function () {
+    await this.list.addWhiteListed(investor, {from: owner}).should.be.fulfilled
+  })
+
+  it('should remove whiteListing by owner', async function () {
+    await this.list.addWhiteListed(investor, {from: owner}).should.be.fulfilled
+    await this.list.removeWhiteListed(investor, {from: owner}).should.be.fulfilled
+  })
+
+})
